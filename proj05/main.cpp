ffdc5425389a0b0f21fb9f8ba682d8899b30da44
Liyang Ye <yeliyang@msu.edu>
2022-08-24 10:35:41 +0800

    Update main.cpp

--- proj05/main.cpp
+++ proj05/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    std::string pass = "password";
+    std::string admin = "intsig";
     return 0;
 }
